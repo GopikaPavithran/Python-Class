1caef16d33ac74f23450dcfa188922cb0e897fed
GopikaPavithran <gopikabindupavithran@gmail.com>
2024-10-06 19:17:23 +0530

    first commit

---
+++ basic programs/user inputs.py
@@ -0,0 +1,14 @@
+n=input('Enter your name:')
+print(n)
+
+a=int(input('Enter your age:'))
+print(a)
+
+s=float(input('Enter your salary:'))
+print(s)
+
+
+
+# input() : string
+# int(input()) : integer
+# float(input()) : float
