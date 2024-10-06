a42bfab23128e06ea0ce1d090a42c7688ba35c07
GopikaPavithran <gopikabindupavithran@gmail.com>
2024-10-07 00:25:17 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	basic programs/operators.py

---
+++ conditional statements/if else.py
@@ -0,0 +1,21 @@
+# syntax
+# if (condition):
+#     body of if
+# else:
+#     body of else
+# eg:
+# n=input("Enter an alphabet:")
+# if(n in "aeiou"):
+#     print('Vowel')
+# else:
+#     print('Not a Vowel')
+
+
+# Q: find largest of two numbers input by the user
+# A:
+a=int(input('Enter first number:'))
+b=int(input('Enter second number:'))
+if(a>b):
+    print(a,"is largest")
+else:
+    print(b,'is largest')
