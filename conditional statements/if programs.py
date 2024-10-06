56dc91e825332d133ab69b1e37b3c74c3c798617
GopikaPavithran <gopikabindupavithran@gmail.com>
2024-10-07 00:07:43 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	basic programs/operators.py

---
+++ conditional statements/if programs.py
@@ -0,0 +1,32 @@
+# syntax
+# if (condition):
+#     body of if
+# eg:
+# n=int(input("Enter a number:"))
+# if(n==10):
+#     print('Hello')
+from multiprocessing.process import ORIGINAL_DIR
+
+
+# # Q: Positive numbers only. print 'Positive'
+# # A:
+# n=int(input("Enter a number:"))
+# if(n>0):
+#     print('Positive')
+
+# # Q: numbers from 1 to 10. print 'Success'
+# # A:
+# n=int(input("Enter a number:"))
+# if(n>=1 and n<=10):
+#     print('Success')
+
+# # Q: a,e,i,o,u. print 'Vowels'
+# # A:
+# n=input("Enter an alphabet:")
+# if(n=='a' or n=='e' or n=='i' or n=='o' or n=='u'):
+#     print('Vowel')
+
+                 # OR
+# n=input("Enter an alphabet:")
+# if(n in "aeiou"):
+#     print('Vowel')
