3de66fcb6fb06f76790b27273fdcdbbc12a4524e
GopikaPavithran <gopikabindupavithran@gmail.com>
2025-02-21 17:25:36 +0530

    feb 21

--- patterns/star pattern.py
+++ patterns/star pattern.py
@@ -1,9 +1,96 @@
-# Q: Right Triangle
-#   *
-#   * *
-#   * * *
-# A:
-for i in range(4):
-    for j in range(i):
-        print('*',end=' ')
-    print()
+# # Q: Right Triangle
+# #   *
+# #   * *
+# #   * * *
+# # A:
+# for i in range(1,4):
+#     for j in range(i):
+#         print('*',end=' ')
+#     print()
+
+
+# # Q: square
+# #   *  *  *
+# #   *  *  *
+# #   *  *  *
+# # A:
+# for i in range(3):
+#     for j in range(3):
+#         print('*',end='  ')
+#     print()
+
+
+# # Q: * * *
+# #    * *
+# #    *
+# # A:
+# for i in range(3):
+#     for j in range(3-i):
+#         print('*',end=' ')
+#     print()
+
+
+# # Q: *
+# #    * *
+# #    * * *
+# #    * * * *
+# #    * * *
+# #    * *
+# #    *
+# # A:
+# for i in range(1,5):
+#     for j in range(i):
+#         print('*',end=' ')
+#     print()
+# for i in range(3):
+#     for j in range(3-i):
+#         print('*',end=' ')
+#     print()
+
+
+# # Q:  *
+# #   *   *
+# # *   *   *
+# # A:
+# for i in range(1,4):
+#     for k in range(3-i):
+#         print(' ',end=' ')
+#     for j in range(i):
+#         print('*',end='   ')
+#     print()
+
+
+# # Q: *   *   *
+# #      *   *
+# #        *
+# # A:
+# for i in range(3):
+#     for k in range(i):
+#         print(' ',end=' ')
+#     for j in range(3-i):
+#         print('*',end='   ')
+#     print()
+
+
+# # Q:    *
+# #     * *
+# #   * * *
+# # A:
+# for i in range(1,4):
+#     for k in range(3-i):
+#         print(' ',end=' ')
+#     for j in range(i):
+#         print('*',end=' ')
+#     print()
+
+
+# # Q: * * *
+# #      * *
+# #        *
+# # A:
+# for i in range(3):
+#     for k in range(i):
+#         print(' ',end=' ')
+#     for j in range(3-i):
+#         print('*',end=' ')
+#     print()
