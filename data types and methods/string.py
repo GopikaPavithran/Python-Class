feebe665eeae916f3a298bbc16a3d44ff47cda36
GopikaPavithran <gopikabindupavithran@gmail.com>
2025-02-24 14:59:41 +0530

    feb 24

--- data types and methods/string.py
+++ data types and methods/string.py
@@ -70,10 +70,10 @@
 
 
 
-# String Checking Methods
-# -----------------------
-
-# 1.isalpha()
+# # String Checking Methods
+# # -----------------------
+# #
+# # 1.isalpha()
 # s=input('Enter a string:')
 # print(s.isalpha())
 
@@ -109,9 +109,43 @@
 
 
 
-# String Iteration
-# ----------------
+# # String Iteration
+# # ----------------
+#
+# a=input("Enter a string:")
+# for i in a:
+#     print(i)
 
-a=input("Enter a string:")
-for i in a:
-    print(i)
+
+# # Q: Write a program to remove space character from a string input by the user without using any function
+# # A:
+# a=input("Enter a string:")
+# for i in a:
+#     if i!=' ':
+#         print(i,end='')
+
+
+
+# # String Slicing
+# # --------------
+#
+# s='welcome to python'
+# print(s[11:17])
+#
+# print(s[:7])          # starting to 6th index
+#
+# print(s[11:])         # 11th index to ending
+#
+# print(s[:])           # full string
+#
+# print(s[::-1])        # reverse
+
+
+# Q: Check a string input by the user is palindrome or not
+# A:
+s=input('Enter a string:')
+if s[::-1]==s:
+    print('Palindrome')
+else:
+    print('Not Palindrome')
+
