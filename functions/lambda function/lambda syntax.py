26796bb97a67ba49c32e20b261c3a45ae8314a4b
GopikaPavithran <gopikabindupavithran@gmail.com>
2025-03-02 23:41:34 +0530

    mar 2

---
+++ functions/lambda function/lambda syntax.py
@@ -0,0 +1,32 @@
+# # Syntax
+# variable=lambda arguments:expression
+
+# product=lambda a,b:(a*b)
+# print(product(4,3))
+
+
+# # Q: Find square root of a number
+# # A:
+# sqrt=lambda n:(n**0.5)
+# print(sqrt(64))
+
+
+# # Q: Find cube of an argument
+# # A:
+# cube=lambda n:n**3
+# print(cube(-3))
+
+
+# # Q: Find area of a triangle
+# # A:
+# area=lambda b,h:(1/2*b*h)
+# print(area(4,6))
+
+
+# # Q: Find area of circle
+# # A:
+# area=lambda r:(3.14*(r**2))
+# print(area(6))
+
+
+
---
+++ functions/lambda function/lambda using if else.py
@@ -0,0 +1,12 @@
+# # Q: Check a number is odd or even
+# # A:
+# x=lambda n:'Even'if(n%2==0)else'Odd'
+# print(x(6))
+# print(x(3))
+
+
+# # Q: Find largest of 2 numbers
+# # A:
+# largest=lambda a,b:a if a>b else b
+# print(largest(4,8))
+# print(largest(98,43))
