0cba284d5a58965b3b2b3c404677d186c29a2215
GopikaPavithran <gopikabindupavithran@gmail.com>
2025-03-21 14:24:11 +0530

    mar 21

--- oops concept/polymorphism/method overloading.py
+++ oops concept/polymorphism/method overloading.py
@@ -18,14 +18,89 @@
 #
 # # @dispatch is a decorator that enables multiple dispatch in Python (@ symbol is essential at the beginning of a decorator's name)
 
+# from multipledispatch import dispatch
+# class Math:
+#     @dispatch(int,int)
+#     def sum(self,a,b):
+#         print(a+b)
+#     @dispatch(int,int,int)
+#     def sum(self,a,b,c):
+#         print(a+b+c)
+# obj=Math()
+# obj.sum(6,3,2)
+# obj.sum(1,3)
+
+
+# # Q: Create product functions
+# # product(1,2)
+# # product(1,2,3)
+# # product(1.5,2.2)
+# # product(1.3,2.1,3.8)
+# # A:
+# from multipledispatch import dispatch
+# class Math:
+#     @dispatch(int,int)
+#     def product(self,a,b):
+#         print(a*b)
+#     @dispatch(int,int,int)
+#     def product(self,a,b,c):
+#         print(a*b*c)
+#     @dispatch(float,float)
+#     def product(self,a,b):
+#         print(a*b)
+#     @dispatch(float,float,float)
+#     def product(self,a,b,c):
+#         print(a*b*c)
+# obj=Math()
+# obj.product(1,2)
+# obj.product(1,2,3)
+# obj.product(1.5,2.2)
+# obj.product(1.3,2.1,3.8)
+
+
+# Q: Create length functions
+# length('abc')
+# length([1,2,3,4])
+# length({'a','b'})
+# length({'Name':'Alex','Age':22})
+# length(('a','b','c'))
+# A:
 from multipledispatch import dispatch
-class Math:
-    @dispatch(int,int)
-    def sum(self,a,b):
-        print(a+b)
-    @dispatch(int,int,int)
-    def sum(self,a,b,c):
-        print(a+b+c)
-obj=Math()
-obj.sum(6,3,2)
-obj.sum(1,3)
+class Length:
+    l=0
+    @dispatch(str)
+    def length(self,st):
+        self.l=0  # Reset length for each call
+        for i in st:
+            self.l+=1
+        print(self.l)
+    @dispatch(list)
+    def length(self,li):
+        self.l=0
+        for i in li:
+            self.l+=1
+        print(self.l)
+    @dispatch(set)
+    def length(self,se):
+        self.l=0
+        for i in se:
+            self.l+=1
+        print(self.l)
+    @dispatch(dict)
+    def length(self,di):
+        self.l=0
+        for i in di:
+            self.l+=1
+        print(self.l)
+    @dispatch(tuple)
+    def length(self,tu):
+        self.l=0
+        for i in tu:
+            self.l+=1
+        print(self.l)
+obj=Length()
+obj.length('abc')
+obj.length([1,2,3,4])
+obj.length({'a','b'})
+obj.length({'Name':'Alex','Age':22})
+obj.length(('a','b','c'))
