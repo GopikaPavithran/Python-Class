1f020d883a5e26efe038c106ab189518146e5bd1
GopikaPavithran <gopikabindupavithran@gmail.com>
2025-03-24 14:59:21 +0530

    Mar 24

---
+++ mysql/database creation.py
@@ -0,0 +1,18 @@
+# # Download XAMPP  - setting up a local server environment
+# # Install pymysql package (pip install pymysql)
+#
+import pymysql
+# mydb=pymysql.connect(host='localhost',user='root',password='')
+# mycursor=mydb.cursor()
+# sql='create database Luminar'
+# mycursor.execute(sql)
+# print('Database Created Successfully')
+
+
+# Q: Create a database using user input
+db=pymysql.connect(host='localhost',user='root',password='')
+cursor=db.cursor()
+dbname=input('Enter your database name:')
+sqlquery='create database %s'%dbname
+cursor.execute(sqlquery)
+print('Database Created successfully')
---
+++ mysql/table creation.py
@@ -0,0 +1,16 @@
+import pymysql
+# mydb=pymysql.connect(host='localhost',user='root',password='',database='luminar')
+# mycursor=mydb.cursor()
+# sql='CREATE TABLE employee_details(id int auto_increment,name varchar(30),salary int(6),primary key(id))'
+# mycursor.execute(sql)
+# mydb.commit()   # to save
+# print('Table Created Successfully')
+
+# Q: Create table student_details with columns id,name,email,qualification,fee,phone number.
+# A:
+mydb=pymysql.connect(host='localhost',user='root',password='',database='luminar')
+mycursor=mydb.cursor()
+sql='CREATE TABLE student_details(id INT AUTO_INCREMENT,name VARCHAR(25),email VARCHAR(50),qualification VARCHAR(50),fee INT(6),phone BIGINT,primary key(id))'
+mycursor.execute(sql)
+mydb.commit()
+print('Table Created Successfully')
